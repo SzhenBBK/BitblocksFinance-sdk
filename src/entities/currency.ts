dc8470230566485e05d409a2138ed18fe6901d43
SzhenBBK <edsoneto.50@gmail.com>
2023-01-23 18:02:17 -0300

    Ajust for polygon

--- src/entities/currency.ts
+++ src/entities/currency.ts
@@ -16,7 +16,7 @@ export class Currency {
   /**
    * The only instance of the base class `Currency`.
    */
-  public static readonly ETHER: Currency = new Currency(18, 'BNB', 'BNB')
+  public static readonly ETHER: Currency = new Currency(18, 'MATIC', 'MATIC')
 
   /**
    * Constructs an instance of the base class `Currency`. The only instance of the base class `Currency` is `Currency.ETHER`.
@@ -32,6 +32,5 @@ export class Currency {
     this.name = name
   }
 }
-
 const ETHER = Currency.ETHER
 export { ETHER }
